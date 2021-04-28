e7bfb04a527b53be52a8832de8f28c11c37e3149
Daniil Boyko <daniil09052000@mail.ru>
2021-04-28 13:54:03 +0300

    Create navigation menu Component

--- components/molecules/Navigation.jsx
+++ components/molecules/Navigation.jsx
@@ -3,22 +3,34 @@ import { useState } from "react"
 
 const Navigation = () => {
 	const [links, setLinks] = useState([
-		{ id: 0, path: "/", name: "Новости" },
-		{ id: 1, path: "/items", name: "Товары" },
-		{ id: 2, path: "/about-us", name: "О нас" },
-		{ id: 3, path: "/contacts", name: "Контакты" }
-	]) 
+		{ id: 1, path: "/", name: "Новости" },
+		{ id: 2, path: "/items", name: "Товары" },
+		{ id: 3, path: "/about-us", name: "О нас" },
+		{ id: 4, path: "/contacts", name: "Контакты" },
+		{ id: 5, path: "/test", name: "Магазин" }
+	])
+	
+	const [selected, setSelected] = useState(0)
+
+	const handleChange = index => {
+		setSelected(index);
+	}
 
 	return (
 		<nav className="navigation">
 			<ul className="navigation__list">
-				{ 
-					links.map(link => (
-						<li className="navigation__item" key={ link.id }>
-							<Link link={ link.path } name={ link.name }/>
-						</li>
-					)) 
-				}
+				{links.map((link, index) => {
+				let style = index === selected ? `navigation__item selected-${link.id}` : "navigation__item";
+				return (
+					<li
+						key={index}
+						className={ style }
+						onClick={() => handleChange(index)}
+					>
+						<Link link={ link.path } name={ link.name }/>
+					</li>
+				);
+			})}
 			</ul>
 		</nav>
 	)
