bac7bde5e7a661b0eeb56f64f83a72f333af8639
Daniil Boyko <daniil09052000@mail.ru>
2021-04-29 15:59:20 +0300

    сonnected api for items

--- components/molecules/ItemsCard.jsx
+++ components/molecules/ItemsCard.jsx
@@ -1,5 +1,4 @@
 import dynamic from "next/dynamic"
-import Image from "next/image"
 import { m } from "framer-motion"
 
 const BadgeButton = dynamic(() => import("../atoms/BadgeButton"))
@@ -7,8 +6,8 @@ const BadgeButton = dynamic(() => import("../atoms/BadgeButton"))
 const ItemsCard = ({title, price, image}) => {
   return (
     <m.div className="items-card" whileHover={{scale: 1.05, transition: "0.5s"}}>
-      <Image className="items-card__image" src="/goods-test.png" alt="Изображение товара" width={268}
-  height={268}/>
+      <img className="items-card__image" src={image} alt="Изображение товара" width={268}
+           height={268}/>
       <h3 className="items-card__title">{title}</h3>
       <BadgeButton price={price}/>
     </m.div>
