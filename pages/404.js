153d5f0cabbc043da754ad0ff6121718fe02a7a3
Daniil Boyko <daniil09052000@mail.ru>
2021-04-30 17:04:35 +0300

    Create 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,20 +1,11 @@
 import Link from "next/link"
-import { useEffect } from "react"
-import { useRouter } from "next/router"
 
 const NotFound = () => {
-  const router = useRouter()
-
-  useEffect(() => {
-    setTimeout(() => {
-      router.push("/")
-    }, 3000)
-  }, [])
-
   return ( 
     <div className="not-found">
-      <h1>Упс, что-то пошло не так</h1>
-      <p>Вернитесь назад - <Link href="/"><a>Домой</a></Link></p>
+      <h1>Страница не найдена.</h1>
+      <span>Воспользуйтесь навигацией для перехода на страницу</span>
+      <Link href="/"><button className="btn btn_medium">На главную</button></Link>
     </div>
    );
 }
