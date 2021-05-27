3491c8cbefe370ca40affe28e0f7aa91233eb048
Sleggi <soltankirill23@gmail.com>
2021-05-27 10:06:58 +0300

    main page

--- layouts/Default.jsx
+++ layouts/Default.jsx
@@ -1,6 +1,8 @@
 import dynamic from "next/dynamic"
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import { useCycle } from 'framer-motion';
+import { useRouter } from 'next/router'
+import AppContext from "../context/appProvider";
 const Navigation = dynamic(() => import("../components/molecules/Navigation"))
 const Cart = dynamic(() => import("../components/molecules/Cart"))
 const BurgerMenu = dynamic(() => import("../components/molecules/BurgerMenu"))
@@ -8,17 +10,24 @@ const BurgerMenu = dynamic(() => import("../components/molecules/BurgerMenu"))
 
 const Default = ({children}) => {
   const size = useWindowSize();
+  const appData = useContext(AppContext);
   const [isOpen, toggleOpen] = useCycle(false, true);
- 
+  const router = useRouter();
+
+  const style = {
+    background: router.pathname === "/" ? `#${appData.settings.theme}` : "#FFFFFF"
+  }
+
+  console.log(router.pathname)
 
   return (
-    <div className="layout">
+    <div className="layout" style={style}>
       <aside>
         <Navigation />
         {/* { size.width <= 1366 ? <BurgerMenu isOpen={isOpen} toggleOpen={toggleOpen} /> : <Navigation /> } */}
       </aside>
       <header>
-      <Cart/>
+        <Cart/>
       </header>
       <main className="container">
         { children }
